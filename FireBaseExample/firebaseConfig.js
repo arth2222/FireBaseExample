428e7043657fb24636c4c1d68bcf0df3a3971322
Per A. Bergersen <pab2222@hotmail.com>
2024-12-12 17:58:01 +0100

    Add project files.

---
+++ FireBaseExample/firebaseConfig.js
@@ -0,0 +1,10 @@
+﻿// firebaseConfig.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyC4DNb1D9EK8TvgwL9PHGEDNLCmAfQZnbs",
+    authDomain: "kantine-2d875.firebaseapp.com",
+    projectId: "kantine-2d875",
+    storageBucket: "kantine-2d875.appspot.com",
+    messagingSenderId: "837764521514",
+    appId: "1:837764521514:web:a9e7dc1326df927ea3ed53",
+    measurementId: "G-CD6C3X6PWP"
+};
